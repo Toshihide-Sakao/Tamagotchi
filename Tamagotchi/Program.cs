ebdbd9aadbb54bbcc26d8a8f90cc98a3407d0bec
Toshihide-Sakao <toshihide.sakao@elev.ga.ntig.se>
2020-10-06 18:12:30 +0900

    done

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -6,7 +6,62 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tamagotchi myTama = new Tamagotchi();
+
+            Console.WriteLine("Name your tamagotchi");
+            myTama.name = Console.ReadLine();
+
+            System.Console.WriteLine($"1. Teach {myTama.name} a new word");
+            System.Console.WriteLine($"2. Say hi to {myTama.name}");
+            System.Console.WriteLine($"3. Give {myTama.name} food");
+            System.Console.WriteLine("4. Don't do anything");
+
+            ConsoleKeyInfo uInput = Console.ReadKey();
+            while (myTama.GetAlive())
+            {
+                switch (uInput.Key)
+                {
+                    case ConsoleKey.D1:
+                        System.Console.WriteLine("The word you want to teach");
+                        string newWord = Console.ReadLine();
+                        myTama.Teach(newWord);
+
+                        System.Console.WriteLine($"You teached her {newWord}");
+
+                        ContinueButton();
+                        myTama.Tick();
+                        break;
+                    case ConsoleKey.D2:
+                        System.Console.WriteLine("Hi!");
+                        myTama.Hi();
+
+                        ContinueButton();
+                        break;
+                    case ConsoleKey.D3:
+                        System.Console.WriteLine($"You fed {myTama.name}");
+                        myTama.Feed();
+
+                        ContinueButton();
+                        myTama.Tick();
+                        break;
+                    case ConsoleKey.D4:
+                        System.Console.WriteLine("You did nothing...");
+
+                        ContinueButton();
+                        myTama.Tick();
+                        break;
+                    default:
+                        System.Console.WriteLine("bRuh");
+                        ContinueButton();
+                        break;
+                }
+            }
+
+            void ContinueButton()
+            {
+                System.Console.WriteLine("\nPress any key to continue");
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tamagotchi
+{
+    public class Tamagotchi
+    {
+        public string name;
+        int hunger;
+        int boredom;
+        List<string> words;
+        bool isAlive;
+        Random generator = new Random();
+
+        public void Feed()
+        {
+            hunger--;
+        }
+        public void Hi()
+        {
+            if (words.Count == 0)
+            {
+                System.Console.WriteLine("Bruh");
+            }
+            else
+            {
+                int rnd = generator.Next(0, words.Count);
+                System.Console.WriteLine($"{name} said {words[rnd]}");
+
+                ReduceBoredom();
+            }
+
+        }
+        public void Teach(string word)
+        {
+            words.Add(word);
+        }
+        public void Tick()
+        {
+            hunger++;
+            boredom++;
+
+            if (hunger == 10 || boredom == 10)
+            {
+                isAlive = false;
+            }
+        }
+        public void PrintStats()
+        {
+            if (isAlive == true)
+                System.Console.WriteLine("Tamagotchi is Alive");
+            else
+                System.Console.WriteLine("Tamagotchi is Dead");
+            System.Console.WriteLine($"\nHunger: {hunger} \nBoredom: {boredom}");
+        }
+        public bool GetAlive()
+        {
+            return isAlive;
+        }
+        void ReduceBoredom()
+        {
+            boredom--;
+        }
+    }
+}
